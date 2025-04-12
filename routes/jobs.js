b42dc9697ef3e5b168b7f98e8ad65b984f2998ae
dhiraj0502 <dhirajkuslekar45@gmail.com>
2025-04-12 10:01:16 +0100

    routes functionality

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,1 +1,24 @@
-routes
+const express = require("express");
+const {
+  getAllJobs,
+  getJob,
+  createJob,
+  updateJob,
+  deleteJob,
+  applyToJob,
+  getApplicants,
+  updateApplicantStatus,
+} = require("../controllers/jobs");
+
+const router = express.Router();
+
+router.get("/", getAllJobs); // Get all jobs
+router.get("/:jobId", getJob); // Get a specific job by ID
+router.post("/", createJob); // Create a new job
+router.patch("/:jobId", updateJob); // Update a job by ID
+router.delete("/:jobId", deleteJob); // Delete a job by ID
+router.post("/apply", applyToJob); // Apply to a job
+router.get("/:jobId/applicants", getApplicants); // Get applicants for a job
+router.patch("/:jobId/applicants/:applicantId", updateApplicantStatus); // Update an applicant's status
+
+module.exports = router;
